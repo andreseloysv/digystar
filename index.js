7cd2b33bc185e6efdcecd20075c0a289e5cde550
Andres Eloy Sanchez Vargas <andres-eloy.sanchez-vargas@deutschebahn.com>
2019-08-10 13:42:38 +0200

    first desing

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const http = require('http');
+const http = require("http");
+fs = require("fs");
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+  fs.readFile("src/index.html", function(error, index) {
+    if (error) {
+      response.writeHead(404);
+      response.write("Contents you are looking are Not Found");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(index);
+    }
+    response.end();
+  });
 });
 
 const port = process.env.PORT || 1337;
