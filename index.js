1e52628fa0bc46786d9d79246cae51698ddfbbaa
Andres Eloy Sanchez Vargas <andres-eloy.sanchez-vargas@deutschebahn.com>
2019-08-23 15:36:26 +0200

    security change to preven sql inyection

--- index.js
+++ index.js
@@ -1,63 +1,60 @@
-const http = require("http");
-const fs = require("fs");
-const parser = require("ua-parser-js");
-const util = require("util");
-const { parse } = require("querystring");
-const requestCountry = require("request-country");
-const RequestModul = require("./modul/request.js");
-const LanguageCode = require("./LanguageCode/languageCode.js");
-const auth = require("http-auth");
-const path = require("path");
+const http = require('http');
+const fs = require('fs');
+const parser = require('ua-parser-js');
+const util = require('util');
+const { parse } = require('querystring');
+const requestCountry = require('request-country');
+const RequestModul = require('./modul/request.js');
+const { MIMETYPE } = require('./globals/constanst.js');
+const LanguageCode = require('./LanguageCode/languageCode.js');
+const auth = require('http-auth');
+const path = require('path');
 
 var basic = auth.basic(
   {
-    realm: "Digystar PrivateArea."
+    realm: 'Digystar PrivateArea.'
   },
   (username, password, callback) => {
     callback(
-      username === "weandresjesusluisare" && password === "therealdigystar"
+      username === 'weandresjesusluisare' && password === 'therealdigystar'
     );
   }
 );
+
+function urlNotFoundError(file, response, contentType, error) {
+  if (error.code == 'ENOENT') {
+    file.readFile('./404.html', function(error, content) {
+      response.writeHead(404, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    });
+  } else {
+    response.writeHead(500);
+    response.end(
+      'Sorry, check with the site admin for error: ' + error.code + ' ..\n'
+    );
+  }
+}
 
 const server = http.createServer(async (request, response) => {
   const userInformation = getUserInformation(request);
   const url = request.url;
 
-  var filePath = "src/" + request.url;
-  if (filePath == "src/") {
-    filePath = "src/index.html";
+  const filePath = 'src/' + request.url;
+  if (filePath == 'src/') {
+    filePath = 'src/index.html';
   }
 
-  var extname = String(path.extname(filePath)).toLowerCase();
-  var mimeTypes = {
-    ".html": "text/html",
-    ".js": "text/javascript",
-    ".css": "text/css",
-    ".json": "application/json",
-    ".png": "image/png",
-    ".jpg": "image/jpg",
-    ".gif": "image/gif",
-    ".wav": "audio/wav",
-    ".mp4": "video/mp4",
-    ".woff": "application/font-woff",
-    ".ttf": "application/font-ttf",
-    ".eot": "application/vnd.ms-fontobject",
-    ".otf": "application/font-otf",
-    ".svg": "application/image/svg+xml",
-    ".wasm": "application/wasm"
-  };
-
-  const contentType = mimeTypes[extname] || "application/octet-stream";
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const contentType = MIMETYPE[extname] || 'application/octet-stream';
 
   const indexFileName = `index${LanguageCode.getLanguageByCountry(
     userInformation.country
   )}`;
 
-  if (url === "/" && request.method === "POST") {
+  if (url === '/' && request.method === 'POST') {
     collectRequestData(request, result => {
-      if (validateEmail(result["user-email"])) {
-        RequestModul.saveUserEmail(result["user-email"]);
+      if (validateEmail(result['user-email'])) {
+        RequestModul.saveUserEmail(result['user-email']);
       }
     });
     const thankyouFileName = `thankyou${LanguageCode.getLanguageByCountry(
@@ -65,73 +62,58 @@ const server = http.createServer(async (request, response) => {
     )}`;
     fs.readFile(`src/${thankyouFileName}.html`, function(error, thankyou) {
       if (error) {
-        response.writeHead(404);
-        response.write("Contents you are looking are Not Found");
+        urlNotFoundError(fs, response, contentType, error);
       } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
+        response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(thankyou);
       }
       response.end();
     });
     return; // not save the request
-  } else if (url === "/favicon.ico" && request.method === "GET") {
-    response.writeHead(200, { "Content-Type": contentType });
-    response.end(content, "utf-8");
+  } else if (url === '/favicon.ico' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(content, 'utf-8');
     return; // not save the request
-  } else if (url === "/stadistics" && request.method === "GET") {
+  } else if (url === '/stadistics' && request.method === 'GET') {
     auth.connect(basic)(request, response, function() {
       fs.readFile(`src/stadistics.html`, function(error, index) {
         if (error) {
-          response.writeHead(404);
-          response.write("Contents you are looking are Not Found");
+          urlNotFoundError(fs, response, contentType, error);
         } else {
-          response.writeHead(200, { "Content-Type": "text/html" });
+          response.writeHead(200, { 'Content-Type': 'text/html' });
           response.write(index);
         }
         response.end();
       });
     });
     return; // not save the request
-  } else if (url === "/stadistics" && request.method === "POST") {
+  } else if (url === '/stadistics' && request.method === 'POST') {
     auth.connect(basic)(request, response, async function() {
-      response.writeHead(200, { "Content-Type": "application/json" });
+      response.writeHead(200, { 'Content-Type': 'application/json' });
       const info = await RequestModul.getRequestInfo();
       response.write(info);
       response.end();
     });
     return; // not save the request
     // Save the info
-  } else if (url === "/" && request.method === "GET") {
+  } else if (url === '/' && request.method === 'GET') {
     fs.readFile(`src/${indexFileName}.html`, function(error, index) {
       if (error) {
-        response.writeHead(404);
-        response.write("Contents you are looking are Not Found");
+        urlNotFoundError(fs, response, contentType, error);
       } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
+        response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(index);
       }
       response.end();
     });
   } else {
     fs.readFile(filePath, function(error, content) {
-      console.log("filePath", filePath);
+      console.log('filePath', filePath);
       if (error) {
-        if (error.code == "ENOENT") {
-          fs.readFile("./404.html", function(error, content) {
-            response.writeHead(404, { "Content-Type": contentType });
-            response.end(content, "utf-8");
-          });
-        } else {
-          response.writeHead(500);
-          response.end(
-            "Sorry, check with the site admin for error: " +
-              error.code +
-              " ..\n"
-          );
-        }
+        urlNotFoundError(fs, response, contentType, error);
       } else {
-        response.writeHead(200, { "Content-Type": contentType });
-        response.end(content, "utf-8");
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
       }
     });
     return; // not save the request
@@ -159,13 +141,13 @@ function validateEmail(email) {
 }
 
 function collectRequestData(request, callback) {
-  const FORM_URLENCODED = "application/x-www-form-urlencoded";
-  if (request.headers["content-type"] === FORM_URLENCODED) {
-    let body = "";
-    request.on("data", chunk => {
+  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+  if (request.headers['content-type'] === FORM_URLENCODED) {
+    let body = '';
+    request.on('data', chunk => {
       body += chunk.toString();
     });
-    request.on("end", () => {
+    request.on('end', () => {
       callback(parse(body));
     });
   } else {
@@ -176,10 +158,10 @@ function collectRequestData(request, callback) {
 function getUserInformation(request) {
   let userInformation = {};
   userInformation.ip =
-    request.headers["x-forwarded-for"] || request.connection.remoteAddress;
+    request.headers['x-forwarded-for'] || request.connection.remoteAddress;
   userInformation.country = requestCountry(request);
   userInformation.host = request.headers.host;
-  const ua = parser(request.headers["user-agent"]);
+  const ua = parser(request.headers['user-agent']);
   userInformation.userAgen = ua.ua;
   userInformation.browserName = ua.browser.name;
   userInformation.browserVersion = ua.browser.version;
@@ -190,11 +172,11 @@ function getUserInformation(request) {
   userInformation.device.vendor = ua.device.vendor;
   userInformation.device.model = ua.device.model;
   userInformation.device.type = ua.device.type;
-  userInformation.acceptLanguage = request.headers["accept-language"];
+  userInformation.acceptLanguage = request.headers['accept-language'];
   return userInformation;
 }
 
 const port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log('Server running at http://localhost:%d', port);
--- modul/request.js
+++ modul/request.js
@@ -1,9 +1,9 @@
-const databaseConnection = require("../globals/db.js");
+const databaseConnection = require('../globals/db.js');
 
-async function queryGetRequestInfo(client, email, password) {
+async function queryGetRequestInfo(client) {
   const query = `SELECT count(id), country FROM "user".request group by country;`;
   const result = await client.query(query);
-  console.log("query request", query);
+  console.log('query request', query);
   return result.rows;
 }
 
@@ -17,12 +17,13 @@ async function getRequestInfo() {
 
 async function querySaveUserEmail(client, email) {
   const query = `INSERT into "user".user 
-    ("email") VALUES ('${email}');`;
-  console.log("query insert user email", query);
-  return await client.query(query);
+    ("email") VALUES ($1);`;
+  const values = [email];
+  console.log('query insert user email', query,values);
+  return await client.query(query, values);
 }
 
-async function saveUserEmail(email = "") {
+async function saveUserEmail(email = '') {
   let client = databaseConnection.getDBClient();
   client.connect();
   await querySaveUserEmail(client, email);
@@ -49,26 +50,40 @@ async function querySaveRequestInfo(
     "browsername", "browserversion",
     "osname","osversion","devicevendor",
     "devicemodel","devicetype",
-    "acceptlanguage") VALUES ('${ip}','${country}',
-    '${host}','${userAgen}','${browserName}','${browserVersion}','${osName}',
-    '${osVersion}','${deviceVendor}','${deviceModel}','${deviceType}','${acceptLanguage}');`;
-  console.log("query insert request", query);
-  return await client.query(query);
+    "acceptlanguage") VALUES ($1,$2,
+      $3,$4,$5,$6,$7,
+      $8,$9,$10,$11,$12);`;
+  const values = [
+    ip,
+    country,
+    host,
+    userAgen,
+    browserName,
+    browserVersion,
+    osName,
+    osVersion,
+    deviceVendor,
+    deviceModel,
+    deviceType,
+    acceptLanguage
+  ];
+  console.log('query insert request', query, values);
+  return await client.query(query, values);
 }
 
 async function saveRequestInfo(
-  ip = "",
-  country = "",
-  host = "",
-  userAgen = "",
-  browserName = "",
-  browserVersion = "",
-  osName = "",
-  osVersion = "",
-  deviceVendor = "",
-  deviceModel = "",
-  deviceType = "",
-  acceptLanguage = ""
+  ip = '',
+  country = '',
+  host = '',
+  userAgen = '',
+  browserName = '',
+  browserVersion = '',
+  osName = '',
+  osVersion = '',
+  deviceVendor = '',
+  deviceModel = '',
+  deviceType = '',
+  acceptLanguage = ''
 ) {
   let client = databaseConnection.getDBClient();
   client.connect();
