2d7dbff1b2fe35109195065d840ea9f344e70374
Andres Eloy Sanchez Vargas <andres-eloy.sanchez-vargas@deutschebahn.com>
2019-12-08 21:01:44 +0100

    Add a better header

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,4 +1,12 @@
 var video = document.getElementById("video");
+const videoHover = document.getElementById("video-hover");
+
+videoHover.addEventListener('click', event => {
+    console.log('video hover');
+    video.play();
+    video.muted = false;
+});
+
 video.addEventListener('click', event => {
     console.log('Hola');
     video.play();
