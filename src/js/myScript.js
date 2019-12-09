11077a64ac1094c8a36838c8f33e845cbb7b6916
Andres Eloy Sanchez Vargas <andres-eloy.sanchez-vargas@deutschebahn.com>
2019-12-09 21:32:48 +0100

    FadeIn and Out when the video start and finish

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,11 +1,12 @@
-var video = document.getElementById("video");
+const video = document.getElementById("video");
 const videoHover = document.getElementById("video-hover");
 
-videoHover.addEventListener('click', handlePlayVideo);
+videoHover.addEventListener('click', handlePlayVideo)
 videoHover.addEventListener('touchstart', handlePlayVideo)
 
 function handlePlayVideo(event){
     console.log('video hover');
+    videoHover.classList.add('animated', 'fadeOut')
     video.play();
     video.muted = false;
 }
@@ -15,3 +16,9 @@ video.addEventListener('click', event => {
     video.play();
     video.muted = false;
 });
+
+video.addEventListener('ended',videoEnd);
+function videoEnd(event) {
+    videoHover.classList.remove("fadeOut");
+    videoHover.classList.add('animated', 'fadeIn')
+}
