f11273b5ea286df5123be168e5453d2ba9269d70
Andres Eloy Sanchez Vargas <andres-eloy.sanchez-vargas@deutschebahn.com>
2019-12-13 17:12:34 +0100

    Add more styles

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,24 +1,49 @@
-const video = document.getElementById("video");
-const videoHover = document.getElementById("video-hover");
+const video = document.getElementById("video")
+const videoHover = document.getElementById("video-hover")
+const getStartedTopButton = document.getElementById("get-started-top-button")
+const inputEmail = document.getElementById("inputEmail")
 
 videoHover.addEventListener('click', handlePlayVideo)
+getStartedTopButton.addEventListener('click', gotToInputEmail)
 videoHover.addEventListener('touchstart', handlePlayVideo)
 
 function handlePlayVideo(event){
-    console.log('video hover');
     videoHover.classList.add('animated', 'fadeOut')
-    video.play();
-    video.muted = false;
+    video.play()
+    video.muted = false
+}
+
+function setFocusInputEmail(){
+    inputEmail.focus();
+}
+
+function gotToInputEmail(event){
+    smoothScroll(0, setFocusInputEmail)
 }
 
 video.addEventListener('click', event => {
-    console.log('Hola');
-    video.play();
-    video.muted = false;
+    video.play()
+    video.muted = false
 });
 
-video.addEventListener('ended',videoEnd);
+video.addEventListener('ended',videoEnd)
+
 function videoEnd(event) {
-    videoHover.classList.remove("fadeOut");
+    videoHover.classList.remove("fadeOut")
     videoHover.classList.add('animated', 'fadeIn')
+    gotToInputEmail()
 }
+
+const smoothScroll = (h, callback) => {
+    let i = h || 0;
+    const step = document.body.scrollHeight/100;
+    if (i < document.body.scrollHeight) {
+      setTimeout(() => {
+        window.scrollTo(0, i);
+        smoothScroll(i + step, callback);
+      }, 10);
+    } else{
+        console.log(callback);
+        callback()
+    }
+  }
