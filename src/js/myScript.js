8a2a489d547f4f62751bc375bc8beea44df04570
Andres Eloy Sanchez Vargas <andres-eloy.sanchez-vargas@deutschebahn.com>
2019-12-17 17:06:33 +0100

    Add faq accordion

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,14 +1,64 @@
+'use strict';
+// Utility section
+
+const addClickAndTouchEventListener = (element, eventHandlerFunction) => {
+    element.addEventListener("click", eventHandlerFunction);
+    element.addEventListener("touch", eventHandlerFunction);
+}
+
+// End of the utility section
+//
+// ---------------------
+//
+// Smooth Scroll section
+
+const smoothScroll = (h, callback) => {
+    let i = h || 0;
+    const step = document.body.scrollHeight/100;
+    if (i < document.body.scrollHeight) {
+      setTimeout(() => {
+        window.scrollTo(0, i);
+        smoothScroll(i + step, callback);
+      }, 10);
+    } else{
+        callback()
+    }
+}
+
+const isInViewport = (element) => {
+    const distance = element.getBoundingClientRect();
+    return ((distance.y - 80) > 0)
+};
+
+const firstFeature = document.getElementById('first-feature');
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', (event) => {
+    window.requestAnimationFrame(changeNavBarColor);
+}, false);
+
+const changeNavBarColor = () => {
+    if (isInViewport(firstFeature)) {
+        header.classList.remove('white')
+        header.classList.add('black')
+    } else {
+        header.classList.add('white')
+        header.classList.remove('black')
+    }
+}
+
+// End of the smooth scroll section
+//
+// -------------
+//
+// Video section
+
 const video = document.getElementById("video")
 const videoHover = document.getElementById("video-hover")
 const getStartedTopButton = document.getElementById("get-started-top-button")
 const inputEmail = document.getElementById("inputEmail")
 
-videoHover.addEventListener('click', handlePlayVideo)
-getStartedTopButton.addEventListener('click', gotToInputEmail)
-videoHover.addEventListener('touchstart', handlePlayVideo)
-getStartedTopButton.addEventListener('touchstart', gotToInputEmail)
-
-async function handlePlayVideo(event){
+const handlePlayVideo = async (event) => {
     videoHover.classList.add('animated', 'fadeOut')
     try{
         await video.play()
@@ -20,12 +70,11 @@ async function handlePlayVideo(event){
     }
 }
 
-function setFocusInputEmail(){
+const setFocusInputEmail = () => {
     inputEmail.focus();
 }
 
-function gotToInputEmail(event){
-    console.log('goToInputEmail')
+const gotToInputEmail = (event) => {
     smoothScroll(0, setFocusInputEmail)
 }
 
@@ -34,48 +83,34 @@ video.addEventListener('click', event => {
     video.muted = false
 });
 
-video.addEventListener('ended',videoEnd)
-
-function videoEnd(event) {
+const videoEnd = (event) => {
     videoHover.classList.remove("fadeOut")
     videoHover.classList.add('animated', 'fadeIn')
     gotToInputEmail()
 }
 
-const smoothScroll = (h, callback) => {
-    let i = h || 0;
-    const step = document.body.scrollHeight/100;
-    if (i < document.body.scrollHeight) {
-      setTimeout(() => {
-        window.scrollTo(0, i);
-        smoothScroll(i + step, callback);
-      }, 10);
-    } else{
-        console.log(callback);
-        callback()
-    }
-  }
+addClickAndTouchEventListener(videoHover, handlePlayVideo)
+addClickAndTouchEventListener(getStartedTopButton, gotToInputEmail)
+video.addEventListener('ended',videoEnd)
 
+// End of the vide section
+//
+// -----------------
+//
+// Accordion section
 
-// holaaa
-const isInViewport = (element) => {
-    const distance = element.getBoundingClientRect();
-    return ((distance.y - 80) > 0)
+const accordions = document.getElementsByClassName("accordion");
+
+const accordionEventHandler = (accordion) => {
+    accordion.classList.toggle("active");
+    const panel = accordion.nextElementSibling;
+    const icon = accordion.firstElementChild;
+    icon.classList.toggle("open");
+    panel.classList.toggle("active");
 };
 
-const firstFeature = document.getElementById('first-feature');
-const header = document.getElementById('header');
+Array.prototype.forEach.call(accordions, (accordion) => {
+    addClickAndTouchEventListener(accordion, accordionEventHandler.bind(undefined, accordion));
+})
 
-window.addEventListener('scroll', function (event) {
-    window.requestAnimationFrame(changeNavBarColor);
-}, false);
-
-function changeNavBarColor(){
-    if (isInViewport(firstFeature)) {
-        header.classList.remove('white')
-        header.classList.add('black')
-    } else {
-        header.classList.add('white')
-        header.classList.remove('black')
-    }
-}
+// End of the accordion section
